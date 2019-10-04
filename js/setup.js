42fdc3e22aa87b070001e3caab67f43ed83cedea
unknown <kz.greenlinefamily@gmail.com>
2019-10-05 01:11:36 +0300

    test

--- js/setup.js
+++ js/setup.js
@@ -73,6 +73,22 @@ for (var i = 0; i < 4; i++) {
   wizards.push(getRandomWizard());
 }
 
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+for (var i = 0; i < 4; i++) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
+
+  similarListElement.appendChild(wizardElement);
+}
+
 
 
 
